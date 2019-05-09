2bbcc8e45f88df29d87c9df24ac2ed07d4be1d66
avinash-git-code <avinash.c.pal@gmail.com>
2019-05-09 09:01:43 +0530

    slider added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,21 +11,14 @@ $(document).ready(function(){
     }
   });
 
-
-    $('.ts_nw_list:first-of-type').slick({
+// Slider 
+    $('.slder-cont ul').slick({
         dots: true,
         infinite: false,
         speed: 500,
-        arrows:false
+        arrows:false,
+        autoPlaySpeed: 3000,
+        autoPlay:true
     });
 
-    $('.carousel_list').slick({
-        autoplay:false,
-        autoPlaySpeed: 5000,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="snp cust-prev"><span></span></button>',
-        nextArrow: '<button type="button" class="snp cust-next"><span></span></button>'
-    });
 });
